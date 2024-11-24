acbf5f2963ac56aea6301ea488dd2e22d35792f2
Lmprobat71 <lmprobat@free.fr>
2024-11-24 22:42:06 +0100

    Update gallery.php

--- html/gallery.php
+++ html/gallery.php
@@ -1,6 +1,16 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $directory = $_SERVER['DOCUMENT_ROOT'] . '/images/Galery/'; // Chemin absolu
 $images = glob($directory . '*.{jpg,jpeg,png,gif}', GLOB_BRACE);
+
+// Vérifiez si des images ont été trouvées
+if ($images === false) {
+    echo json_encode(['error' => 'Erreur lors de la lecture du dossier']);
+    exit;
+}
 
 // Convertir le chemin absolu en chemin relatif pour le client
 $images = array_map(function($image) {
